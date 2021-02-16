9cf2cfdde9c12b27e3c93d2a5f32f28876f42d65
Ashot Siroyan <ashotsiroyan0@gmail.com>
2021-02-16 23:26:28 +0400

    v1.4.7

--- lib/Model.d.ts
+++ lib/Model.d.ts
@@ -1,8 +1,8 @@
 import Document from './Document';
-import DocumentQuery from './DocumentQuery';
+import { DocumentQuery } from './Query';
 import Schema from './Schema';
 import Connection from './Connection';
-declare type QuerySelector = {
+export declare type QuerySelector = {
     $eq?: any;
     $gt?: any;
     $gte?: any;
@@ -12,31 +12,26 @@ declare type QuerySelector = {
     $ne?: any;
     $nin?: string | string[] | number | number[];
 };
-declare type RootQuerySelector = {
+export declare type RootQuerySelector = {
     _id?: string;
     $or?: Array<FilterQuery>;
     $and?: Array<FilterQuery>;
 };
-declare type FilterQuery = {
+export declare type FilterQuery = {
     [field: string]: QuerySelector | string | number;
 };
-export interface DocProps {
-    schema: Schema;
-    db: Connection;
-    modelName: string;
-}
 interface IModel<T extends Document> {
-    new: (doc?: any) => Document;
+    new: (doc?: any) => T;
     find(conditions?: RootQuerySelector | FilterQuery, fields?: string[]): DocumentQuery<T[], T>;
-    find(conditions: RootQuerySelector | FilterQuery, fields: string[], callback: (err: any, res?: Document[]) => void): DocumentQuery<T[], T>;
+    find(conditions: RootQuerySelector | FilterQuery, fields: string[], callback: (err: any, res?: T[]) => void): DocumentQuery<T[], T>;
     findOne(conditions?: RootQuerySelector | FilterQuery, fields?: string[]): DocumentQuery<T | null, T>;
-    findOne(conditions: RootQuerySelector | FilterQuery, fields: string[], callback: (err: any, res?: Document) => void): DocumentQuery<T | null, T>;
+    findOne(conditions: RootQuerySelector | FilterQuery, fields: string[], callback: (err: any, res?: T) => void): DocumentQuery<T | null, T>;
     findById(id: string, fields?: string[]): DocumentQuery<T | null, T>;
-    findById(id: string, fields: string[], callback: (err: any, res?: Document) => void): DocumentQuery<T | null, T>;
-    insertOne(params: object): Promise<Document>;
-    insertOne(params: object, callback: (err: any, res?: Document) => void): void;
-    insertMany(params: object[]): Promise<Document[]>;
-    insertMany(params: object[], callback: (err: any, res?: Document[]) => void): void;
+    findById(id: string, fields: string[], callback: (err: any, res?: T) => void): DocumentQuery<T | null, T>;
+    insertOne(params: object): Promise<T>;
+    insertOne(params: object, callback: (err: any, res?: T) => void): void;
+    insertMany(params: object[]): Promise<T[]>;
+    insertMany(params: object[], callback: (err: any, res?: T[]) => void): void;
     updateOne(conditions: RootQuerySelector | FilterQuery, update: any): Promise<any>;
     updateOne(conditions: RootQuerySelector | FilterQuery, update: any, callback: (err: any, raw?: any) => void): void;
     updateMany(conditions: RootQuerySelector | FilterQuery, update: any): Promise<any>;
@@ -49,28 +44,27 @@ interface IModel<T extends Document> {
     countDocuments(conditions: RootQuerySelector | FilterQuery, callback: (err: any, res?: number) => void): void;
 }
 declare class Model<T extends Document> implements IModel<T> {
-    private docProps;
     readonly schema: Schema;
     readonly db: Connection;
+    readonly modelName: string;
     constructor(name: string, schema: Schema, db: Connection);
-    get modelName(): string;
-    new(doc?: any): Document;
-    find(callback?: (err: any, res?: Document[]) => void): DocumentQuery<T[], T>;
-    find(conditions: RootQuerySelector | FilterQuery, callback?: (err: any, res?: Document[]) => void): DocumentQuery<T[], T>;
-    find(conditions: RootQuerySelector | FilterQuery, fields?: string[], callback?: (err: any, res?: Document[]) => void): DocumentQuery<T[], T>;
-    findOne(callback?: (err: any, res?: Document) => void): DocumentQuery<T | null, T>;
-    findOne(conditions: RootQuerySelector | FilterQuery, callback?: (err: any, res?: Document) => void): DocumentQuery<T | null, T>;
-    findOne(conditions: RootQuerySelector | FilterQuery, fields?: string[], callback?: (err: any, res?: Document) => void): DocumentQuery<T | null, T>;
-    findById(id: string, callback?: (err: any, res?: Document) => void): DocumentQuery<T | null, T>;
-    findById(id: string, fields?: string[], callback?: (err: any, res?: Document) => void): DocumentQuery<T | null, T>;
-    insertOne(params: object): Promise<Document>;
+    new(doc?: any): T;
+    find(callback?: (err: any, res?: T[]) => void): DocumentQuery<T[], T>;
+    find(conditions: RootQuerySelector | FilterQuery, callback?: (err: any, res?: T[]) => void): DocumentQuery<T[], T>;
+    find(conditions: RootQuerySelector | FilterQuery, fields?: string[], callback?: (err: any, res?: T[]) => void): DocumentQuery<T[], T>;
+    findOne(callback?: (err: any, res?: T) => void): DocumentQuery<T | null, T>;
+    findOne(conditions: RootQuerySelector | FilterQuery, callback?: (err: any, res?: T) => void): DocumentQuery<T | null, T>;
+    findOne(conditions: RootQuerySelector | FilterQuery, fields?: string[], callback?: (err: any, res?: T) => void): DocumentQuery<T | null, T>;
+    findById(id: string, callback?: (err: any, res?: T) => void): DocumentQuery<T | null, T>;
+    findById(id: string, fields?: string[], callback?: (err: any, res?: T) => void): DocumentQuery<T | null, T>;
+    insertOne(params: object): Promise<T>;
     insertOne(params: object, callback: (err: any, res?: Document) => void): void;
-    insertMany(params: object[]): Promise<Document[]>;
-    insertMany(params: object[], callback: (err: any, res?: Document[]) => void): void;
-    updateOne(conditions: RootQuerySelector | FilterQuery, update: any): Promise<any>;
-    updateOne(conditions: RootQuerySelector | FilterQuery, update: any, callback: (err: any, raw?: any) => void): void;
-    updateMany(conditions: RootQuerySelector | FilterQuery, update: any): Promise<any>;
-    updateMany(conditions: RootQuerySelector | FilterQuery, update: any, callback: (err: any, raw?: any) => void): void;
+    insertMany(params: object[]): Promise<T[]>;
+    insertMany(params: object[], callback: (err: any, res?: T[]) => void): void;
+    updateOne(conditions: RootQuerySelector | FilterQuery, doc: any): Promise<any>;
+    updateOne(conditions: RootQuerySelector | FilterQuery, doc: any, callback: (err: any, raw?: any) => void): void;
+    updateMany(conditions: RootQuerySelector | FilterQuery, doc: any): Promise<any>;
+    updateMany(conditions: RootQuerySelector | FilterQuery, doc: any, callback: (err: any, raw?: any) => void): void;
     deleteOne(conditions?: RootQuerySelector | FilterQuery): Promise<void>;
     deleteOne(conditions: RootQuerySelector | FilterQuery, callback: (err: any) => void): void;
     deleteMany(conditions?: RootQuerySelector | FilterQuery): Promise<void>;
--- lib/Query.d.ts
+++ lib/Query.d.ts
@@ -3,7 +3,7 @@ import Model from './Model';
 declare type SortType = {
     [field: string]: -1 | 1;
 };
-declare class DocumentQuery<T, DocType extends Document> {
+export declare class DocumentQuery<T, DocType extends Document> {
     private mainQuery;
     private skipQuery;
     private sortQuery;
@@ -18,4 +18,9 @@ declare class DocumentQuery<T, DocType extends Document> {
     exec(callback: (err: any, res?: T) => void): void;
     private checkDb;
 }
-export default DocumentQuery;
+export declare class Query {
+    readonly model: Model<any>;
+    constructor(model: Model<any>);
+    update(doc: any): string;
+}
+export {};
