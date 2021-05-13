8ad1b295cee454ca761cd7d37e7d5c27ac9a861e
Ashot Siroyan <ashotsiroyan0@gmail.com>
2021-05-13 19:03:33 +0400

    Added Union method of Model

--- lib/Query.d.ts
+++ lib/Query.d.ts
@@ -1,19 +1,22 @@
 import Document from './Document';
-import Model from './Model';
+import Model, { RootQuerySelector, FilterQuery } from './Model';
 declare type SortType = {
     [field: string]: -1 | 1;
 };
 export declare class DocumentQuery<T, DocType extends Document> {
-    private mainQuery;
+    private conditions;
+    private fields;
     private skipQuery;
     private sortQuery;
     private limitQuery;
     private isOne;
+    private unionModels;
     readonly model: Model<DocType>;
-    constructor(query: string, model: Model<DocType>, isOne?: boolean);
+    constructor(conditions: RootQuerySelector | FilterQuery, fields: string[], model: Model<DocType>, isOne?: boolean);
     limit(val: number | string): this;
     skip(val: number | string): this;
     sort(arg: SortType): this;
+    union(model: Model<DocType> | Model<DocType>[], all?: boolean): this;
     exec(): Promise<T>;
     exec(callback: (err: any, res?: T) => void): void;
 }
